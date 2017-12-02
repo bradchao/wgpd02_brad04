bc4a6d19cbab9262f58c1c7532e7796766a16ee5
brad <brad@brad.tw>
2017-12-02 14:35:52 +0800

    201712021435

--- src/test5.js
+++ src/test5.js
@@ -1,7 +1,7 @@
 
 var Test5Layer = cc.Layer.extend({
     ball:null,
-    var2: 123,
+    var2: 147,
     ctor:function () {
         this._super();
 
@@ -11,17 +11,45 @@ var Test5Layer = cc.Layer.extend({
         this.addChild(this.ball);
 
         this.ball.schedule(
-            this.balltask, 1, cc.REPEAT_FOREVER, 1);
+            this.balltask, 0.01, cc.REPEAT_FOREVER, 1);
+
+        this.initListener();
 
 
         return true;
+    },
+
+    initListener: function () {
+        var myMouseListener = {
+            event: cc.EventListener.MOUSE,
+            onMouseDown: function (event) {
+                var layer = event.getCurrentTarget();
+                layer.addBall(event.getLocationX(), event.getLocationY());
+            }
+        };
+        cc.eventManager.addListener(myMouseListener, this);
+    },
+
+    addBall: function (x, y) {
+        cc.log('addBall:' + x + " x " + y);
     },
 
     balltask: function () {
         // this => who schedule
         var layer = this.getParent();
 
-        
+        if (this.x - this.width/2 < 0 ||
+        this.x + this.width/2 > cc.winSize.width){
+            this.dx *= -1;
+        }
+        if (this.y - this.height/2 < 0 ||
+        this.y + this.height/2 > cc.winSize.height){
+            this.dy *= -1;
+        }
+
+        this.x += this.dx;
+        this.y += this.dy;
+
 
 
     }
