71312d830179c4335adbc7e92ae027ac863842a9
brad <brad@brad.tw>
2017-12-03 10:40:49 +0800

    201712031040

--- src/test5.js
+++ src/test5.js
@@ -2,7 +2,7 @@
 var Test5Layer = cc.Layer.extend({
     balls: [res.ball0, res.ball1, res.ball2, res.ball3, res.ball4],
     isPause: false,
-    bricks: [],
+    bricks: [[]],
     ctor:function () {
         this._super();
 
@@ -13,11 +13,14 @@ var Test5Layer = cc.Layer.extend({
     },
 
     initBrick: function () {
-        for (var i=0; i<20; i++){
-            this.bricks[i] = new cc.Sprite(res.brick);
-            this.bricks[i].x = 24 + this.bricks[i].width * i;
-            this.bricks[i].y = cc.winSize.height - 24;
-            this.addChild(this.bricks[i]);
+        for (var j=0; j<4; j++){
+            this.bricks[j] = [];
+            for (var i=0; i<20; i++){
+                this.bricks[j][i] = new cc.Sprite(res.brick);
+                this.bricks[j][i].x = 24 + this.bricks[j][i].width * i;
+                this.bricks[j][i].y = cc.winSize.height - (j+2)*48;
+                this.addChild(this.bricks[j][i]);
+            }
         }
     },
 
@@ -53,8 +56,8 @@ var Test5Layer = cc.Layer.extend({
         //cc.log('addBall:' + x + " x " + y);
         var ball = new Ball(
             this.balls[parseInt(Math.random()*5)],
-            parseInt(Math.random()*24) - 12,
-            parseInt(Math.random()*24) - 12
+            parseInt(Math.random()*48) - 24,
+            parseInt(Math.random()*48) - 24
         );
         ball.x = x;
         ball.y = y;
@@ -70,65 +73,55 @@ var Test5Layer = cc.Layer.extend({
 
         if (layer.isPause) return;
 
+        brad:
+        for (var j=layer.bricks.length-1; j>=0; j--) {
+            for (var i = 0; i < layer.bricks[j].length; i++) {
+                var leftX = layer.bricks[j][i].x - layer.bricks[j][i].width / 2;
+                var rightX = layer.bricks[j][i].x + layer.bricks[j][i].width / 2;
+                var upY = layer.bricks[j][i].y + layer.bricks[j][i].height / 2;
+                var downY = layer.bricks[j][i].y - layer.bricks[j][i].height / 2;
 
+                if (this.x <= rightX && this.x >= leftX &&
+                    ((this.y > layer.bricks[j][i].y && this.y - this.height / 2 <= upY) ||
+                        (this.y < layer.bricks[j][i].y && this.y + this.height / 2 >= downY))
+                ) {
+                    // 球碰到上下緣
+                    layer.removeChild(layer.bricks[j][i]);
+                    layer.bricks[j].splice(i, 1);
+                    this.dy *= -1;
+                    break brad;
+                }
 
-        for (var i = 0; i<layer.bricks.length; i++){
-            if (this.x<=layer.bricks[i].x + layer.bricks[i].width/2 &&
-                    this.x>=layer.bricks[i].x-layer.bricks[i].width/2 &&
-                ((this.y>layer.bricks[i].y &&
-                    this.y-this.height/2<=layer.bricks[i].y+layer.bricks[i].height/2 ) ||
-                    (this.y<layer.bricks[i].y &&
-                    this.y+this.height/2>=layer.bricks[i].y-layer.bricks[i].height/2))
-            ){
-                // 球碰到上下緣
-                layer.removeChild(layer.bricks[i]);
-                layer.bricks.splice(i,1);
-                this.dy *= -1;
-                break;
+                if (this.y <= upY && this.y >= downY &&
+                    ((this.x < layer.bricks[j][i].x && this.x + this.width / 2 >= leftX) ||
+                        (this.x > layer.bricks[j][i].x && this.x - this.width / 2 <= rightX))
+                ) {
+                    // 左右邊碰撞
+                    layer.removeChild(layer.bricks[j][i]);
+                    layer.bricks[j].splice(i, 1);
+                    this.dx *= -1;
+                    break brad;
+                }
+
+                var p0 = new cc.Point(leftX, upY);
+                var p1 = new cc.Point(leftX, downY);
+                var p2 = new cc.Point(rightX, upY);
+                var p3 = new cc.Point(rightX, downY);
+                var thisp = new cc.Point(this.x, this.y);
+
+                if (cc.pDistance(p0, thisp) < this.width / 2 ||
+                    cc.pDistance(p1, thisp) < this.width / 2 ||
+                    cc.pDistance(p2, thisp) < this.width / 2 ||
+                    cc.pDistance(p3, thisp) < this.width / 2) {
+                    // 角邊碰撞
+                    layer.removeChild(layer.bricks[j][i]);
+                    layer.bricks[j].splice(i, 1);
+                    this.dx *= -1;
+                    this.dy *= -1;
+                    break brad;
+                }
             }
-
-            if ( this.y<=layer.bricks[i].y+layer.bricks[i].height/2 &&
-                 this.y>=layer.bricks[i].y-layer.bricks[i].height/2 &&
-                ((this.x<layer.bricks[i].x &&
-                    this.x+this.width/2 >= layer.bricks[i].x-layer.bricks[i].width/2) ||
-                    (this.x>layer.bricks[i].x &&
-                    this.x-this.width/2 <= layer.bricks[i].x+layer.bricks[i].width/2 ))
-            ){
-                // 左右邊碰撞
-                layer.removeChild(layer.bricks[i]);
-                layer.bricks.splice(i,1);
-                this.dx *= -1;
-                break;
-            }
-
-            var leftX = layer.bricks[i].x - layer.bricks[i].width/2;
-            var rightX = layer.bricks[i].x + layer.bricks[i].width/2;
-            var upY = layer.bricks[i].y + layer.bricks[i].height/2;
-            var downY = layer.bricks[i].y - layer.bricks[i].height/2;
-
-            var p0 = new cc.Point(leftX, upY);
-            var p1 = new cc.Point(leftX, downY);
-            var p2 = new cc.Point(rightX, upY);
-            var p3 = new cc.Point(rightX, downY);
-            var thisp = new cc.Point(this.x, this.y);
-
-            if (cc.pDistance(p0,thisp) < this.width/2 ||
-                cc.pDistance(p1,thisp) < this.width/2 ||
-                cc.pDistance(p2,thisp) < this.width/2 ||
-                cc.pDistance(p3,thisp) < this.width/2) {
-                // 角邊碰撞
-                layer.removeChild(layer.bricks[i]);
-                layer.bricks.splice(i,1);
-                this.dx *= -1;
-                this.dy *= -1;
-                break;
-            }
-
-
         }
-
-
-
 
         if (this.x - this.width/2 < 0 ||
         this.x + this.width/2 > cc.winSize.width){
